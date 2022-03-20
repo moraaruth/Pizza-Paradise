dd9b073f72a30a25c798ccbd6cc27f2edbf31f60
Ruth Moraa <moraar37@gmail.com>
2022-03-20 17:51:57 +0300

    proper indentation and spacing

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
+
     $("#buy").click(function () {
 
+        $("#yourorder").show();
+
         var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
+
         var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
+
         var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
+
         var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
 
 
         $("#pizzselect").html($("#inlineFormCustomSelect1 option:selected").text() + "-" + sizeOfPizza);
+
         $("#toppselect").html($("#inlineFormCustomSelect2 option:selected").text() + "-" + toppingsOfPizza);
+
         $("#crusttselect").html($("#inlineFormCustomSelect3 option:selected").text() + "-" + crustOfPizza);
+
         $("#total").html(total);
 
 
@@ -18,36 +27,57 @@ $(document).ready(function () {
 });
 
 $("#add").click(function () {
+
     var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
+
     var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
+
     var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
+
     var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+
     var order = 1;
+
     var grandTotal = 0;
 
 
     $("#pizzselect").html($("#inlineFormCustomSelect1 option:selected").text() + " - " + sizeOfPizza);
+
     $("#toppselect").html($("#inlineFormCustomSelect2 option:selected").text() + " - " + toppingsOfPizza);
+
     $("#crusttselect").html($("#inlineFormCustomSelect3 option:selected").text() + " - " + crustOfPizza);
+
     $("#total").html(total);
 
     class Pizza {
+
         constructor(size, toppings, crust, total, orderNo) {
+
             this.size = size;
+
             this.toppings = toppings;
+
             this.crust = crust;
+
             this.total = total;
+
             this.orderNo = orderNo;
         }
     }
 
 
     $("#add").click(function () {
+
         var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
+
         var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
+
         var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
+
         var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+
         order = order + 1;
+
         grandTotal = grandTotal + total;
 
 
@@ -56,14 +86,19 @@ $("#add").click(function () {
         var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
 
         $("#Pizza").append(newRow);
+
     });
 
 });
 
 $("#checkout").click(function () {
+
     $("#add").hide();
+
     $("#checkout").hide();
+
     $("#price").hide();
+
     grandTotal = grandTotal + total;
 
     $("#totalbill").html(grandTotal + 250);
