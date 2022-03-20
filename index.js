1ebc6b044139462acc7f7a6a260f8f47995f8fbf
Ruth Moraa <moraar37@gmail.com>
2022-03-20 15:27:01 +0300

    adding first order jquery function

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 $(document).ready(function () {
     $("#buy").click(function () {
 
-        var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").text();
-        var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").text();
-        var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").text();
-        var total = (sizeOfPizza + toppingsOfPizza + crustOfPizza);
+        var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
+        var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
+        var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
+        var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
 
 
-        alert("value:" + total);
-
-
-
-
-
-
-
-
-
-
-
+        $("#pizzselect").html($("#inlineFormCustomSelect1 option:selected").text() + "-" + sizeOfPizza);
+        $("#toppselect").html($("#inlineFormCustomSelect2 option:selected").text() + "-" + toppingsOfPizza);
+        $("#crusttselect").html($("#inlineFormCustomSelect3 option:selected").text() + "-" + crustOfPizza);
+        $("#total").html(total);
 
 
     });
+
 });
+
+
+
 
 
 
