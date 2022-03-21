e937f59a13f85afbf8b7fa45fefa342c9d909c33
Ruth Moraa <moraar37@gmail.com>
2022-03-21 07:51:24 +0300

    merge conflict

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ $(document).ready(function () {
         var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza) + parseInt(addToppingsOfPizza);
 
         var order = 1;
-<<<<<<< HEAD
 
         var grandTotal = 0;
 
@@ -31,24 +30,10 @@ $(document).ready(function () {
         $("#total").html(total);
 
 
-=======
-
-        var grandTotal = 0;
-
-
-        $("#pizzselect").html($("#inlineFormCustomSelect1 option:selected").text() + "-" + sizeOfPizza);
-
-        $("#toppselect").html($("#inlineFormCustomSelect2 option:selected").text() + "-" + toppingsOfPizza);
-
-        $("#crusttselect").html($("#inlineFormCustomSelect3 option:selected").text() + "-" + crustOfPizza);
-
-        $("#addToppselect").html($("#topp1 input").text() + "-" + addToppingsOfPizza);
->>>>>>> master
 
         $("#total").html(total);
 
 
-<<<<<<< HEAD
         class paradise {
 
             constructor(size, crust, toppings, addToppings, total, orderNo) {
@@ -69,30 +54,9 @@ $(document).ready(function () {
 
 
         $("#add").click(function () {
-=======
-
-
-
-        class paradise {
-
-            constructor(size, crust, toppings, total, orderNo) {
-
-                this.size = size;
-
-                this.crust = crust;
-
-                this.toppings = toppings;
-
-                this.total = total;
-
-                this.orderNo = orderNo;
-            }
-        }
->>>>>>> master
 
             var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
 
-<<<<<<< HEAD
             var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
 
             var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
@@ -105,37 +69,14 @@ $(document).ready(function () {
 
             grandTotal = grandTotal + total;
 
-=======
-        $("#add").click(function () {
-
-            var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
-
-            var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
-
-            var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
-
-            var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-
-            order = order + 1;
-
-            grandTotal = grandTotal + total;
->>>>>>> master
 
             var newParadise = new paradise(sizeOfPizza, toppingsOfPizza, crustOfPizza, addToppingsOfPizza, total, order);
 
-<<<<<<< HEAD
             var newRow = '<tr><th scope="row">' + newParadise.orderNo + '</th><td id="pizzselect">' + $("#inlineFormCustomSelect1 option:selected").text() +
                 " - " + newParadise.size + '</td><td id="toppselect">' + $("#inlineFormCustomSelect2 option:selected").text() + " - " + newParadise.toppings +
                 '</td><td id="crusttselect">' + $("#inlineFormCustomSelect3 option:selected").text() + " - " + newParadise.crust + '</td><td id="addToppselect">' + $("#topp1 input").text() + " - " + newParadise.addToppings +
                 '</td><td id="total">' + newParadise.total + '</td></tr>'
 
-=======
-            var newParadise = new paradise(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
-
-            var newRow = '<tr><th scope="row">' + newParadise.orderNo + '</th><td id="pizzselect">' + $("#inlineFormCustomSelect1 option:selected").text() +
-                " - " + newParadise.size + '</td><td id="toppselect">' + $("#inlineFormCustomSelect2 option:selected").text() + " - " + newParadise.toppings + '</td><td id="crusttselect">' + $("#inlineFormCustomSelect3 option:selected").text() + " - " + newParadise.crust + '</td><td id="total">' + newParadise.total + '</td></tr>'
-
->>>>>>> master
             $("#paradise").append(newRow)
         });
 
