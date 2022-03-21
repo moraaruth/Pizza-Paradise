18da4f3a341a98f93f87007003adf77809668053
Ruth Moraa <moraar37@gmail.com>
2022-03-21 05:43:10 +0300

    renaming of sections

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ $(document).ready(function () {
 
         var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
 
+        var order = 1;
+
+        var grandTotal = 0;
+
 
         $("#pizzselect").html($("#inlineFormCustomSelect1 option:selected").text() + "-" + sizeOfPizza);
 
@@ -22,76 +26,52 @@ $(document).ready(function () {
         $("#total").html(total);
 
 
-    });
-
-});
-
-$("#add").click(function () {
-
-    var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
-
-    var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
-
-    var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
-
-    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-
-    var order = 1;
-
-    var grandTotal = 0;
 
 
-    $("#pizzselect").html($("#inlineFormCustomSelect1 option:selected").text() + " - " + sizeOfPizza);
 
-    $("#toppselect").html($("#inlineFormCustomSelect2 option:selected").text() + " - " + toppingsOfPizza);
+        class paradise {
 
-    $("#crusttselect").html($("#inlineFormCustomSelect3 option:selected").text() + " - " + crustOfPizza);
+            constructor(size, crust, toppings, total, orderNo) {
 
-    $("#total").html(total);
+                this.pizsize = size;
 
-    class Pizza {
+                this.crust = crust;
 
-        constructor(size, toppings, crust, total, orderNo) {
+                this.toppings = toppings;
 
-            this.size = size;
+                this.total = total;
 
-            this.toppings = toppings;
-
-            this.crust = crust;
-
-            this.total = total;
-
-            this.orderNo = orderNo;
+                this.orderNo = orderNo;
+            }
         }
-    }
 
 
-    $("#add").click(function () {
+        $("#add").click(function () {
 
-        var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
+            var sizeOfPizza = $("#inlineFormCustomSelect1 option:selected").val();
 
-        var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
+            var toppingsOfPizza = $("#inlineFormCustomSelect2 option:selected").val();
 
-        var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
+            var crustOfPizza = $("#inlineFormCustomSelect3 option:selected").val();
 
-        var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+            var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
 
-        order = order + 1;
+            order = order + 1;
 
-        grandTotal = grandTotal + total;
+            grandTotal = grandTotal + total;
 
 
-        var newPizza = new Pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
+            var newParadise = new paradise(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
 
-        var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $("#inlineFormCustomSelect1 option:selected").text()
-            + " - " + newPizza.size + '</td><td id="toppselect">' + $("#inlineFormCustomSelect2 option:selected").text() + " - " + newPizza.toppings + '</td><td id="crusttselect">' + $("#inlineFormCustomSelect3 option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+            var newRow = '<tr><th scope="row">' + newParadise.orderNo + '</th><td id="pizzselect">' + $("#inlineFormCustomSelect1 option:selected").text() +
+                " - " + newParadise.size + '</td><td id="toppselect">' + $("#inlineFormCustomSelect2 option:selected").text() + " - " + newParadise.toppings + '</td><td id="crusttselect">' + $("#inlineFormCustomSelect3 option:selected").text() + " - " + newParadise.crust + '</td><td id="total">' + newParadise.total + '</td></tr>'
 
-        $("newPizza").append(newRow)
+            $("#paradise").append(newRow)
+        });
+
     });
 
 });
-
-
 
 
 
@@ -120,11 +100,3 @@ $("#placeorder").click(function () {
 
 
 });
-
-
-
-
-
-
-
-
